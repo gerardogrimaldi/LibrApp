aad53814271339494ae9596f5f07f5ff9f6ebb93
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2014-01-11 15:03:48 -0300

    fiixed post on angular

--- app/scripts/controllers/user/create.js
+++ app/scripts/controllers/user/create.js
@@ -3,7 +3,7 @@
 angular.module('LibrApp').controller('UserCreateCtrl', function ($scope, $http) {
 
   	$scope.createUser = function() {
-		$http({method: 'POST', data : $scope.user, url: 'https://librappapiserver-c9-gerardogrimaldi.c9.io/api/create/' }).//'http://api.gerardogrimaldi.com/api/user/create/'}).
+		$http({method: 'POST', data : JSON.stringify($scope.user), url: 'https://newlibrappapiserver-c9-gerardogrimaldi.c9.io/api/user/' }).//'http://api.gerardogrimaldi.com/api/user/create/'}).
 			success(function(data, status, headers, config) {
 			// this callback will be called asynchronously
 			// when the response is available
