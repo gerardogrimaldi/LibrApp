a03d957f3651a99914c484373bd1a6f32f463ec3
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2014-01-19 20:05:06 -0300

    edit modal

--- app/scripts/controllers/user/show.js
+++ app/scripts/controllers/user/show.js
@@ -2,10 +2,10 @@
 
 angular.module('LibrApp').controller('UserShowCtrl', function ($scope, $http) {
 
-    $scope.loadUsers = function() {
+    $scope.queryUsers = function() {
         $http({method: 'GET', data : JSON.stringify($scope.user), url: 'https://librappapiserver1-c9-gerardogrimaldi.c9.io/api/users/' }).//'http://api.gerardogrimaldi.com/api/user/create/'}).
             success(function(data, status, headers, config) {
-                $scope.model = data;
+                $scope.users = data;
                 toastr.success("Cargando datos...");
             }).
             error(function(data, status, headers, config) {
@@ -13,16 +13,26 @@ angular.module('LibrApp').controller('UserShowCtrl', function ($scope, $http) {
             });
         }
 
-    $scope.loadUsers();
+    $scope.queryUsers();
 
+    $scope.editUser = function(club) {
+        $scope.myDialog = $dialog.dialog({dialogFade: false, resolve: {user: function(){return angular.copy(user);}}});
+        $scope.myDialog.open('club/club_edit.tpl.html', 'ClubEditCtrl').then(function(result){
+            if (result === 'cancel'){}
+            else {
+                 $scope.queryUsers();
+                //$scope.users = ClubRes.query();
+            }
+        });  
+    };
     $scope.gridOptions = { 
-        data: 'model',
+        data: 'users',
         columnDefs: [
-        {field:'name', displayName:'Name'}, 
-        {field:'email', displayName:'E-mail'},
-        {displayName: 'Edit', cellTemplate: '<button id="editBtn" type="button" class="btn btn-primary" ng-click="editTeam(row.entity)" >Edit</button> '},
-      {displayName: 'Delete', cellTemplate: '<button id="deleteBtn" type="button" class="btn btn-primary" ng-click="deleteTeam(row.entity)" >Delete</button> '}
+            {field:'name', displayName:'Name'}, 
+            {field:'email', displayName:'E-mail'},
+            {displayName: 'Edit', cellTemplate: '<button id="editBtn" type="button" class="btn btn-primary" ng-click="edit(row.entity)" >Edit</button> '},
+            {displayName: 'Delete', cellTemplate: '<button id="deleteBtn" type="button" class="btn btn-primary" ng-click="delete(row.entity)" >Delete</button> '}
         ],
-        
+        multiSelect: false,
     };
 });
