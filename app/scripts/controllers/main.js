a939127bc8672264c1a10a45c4552b971baac4df
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2014-03-13 22:52:23 -0300

    adding semantic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,13 @@
 
 angular.module('LibrApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.MainMessage = [
-      'Hola'
-    ];
+  	$('.login').popup({
+	  	on: 'click',
+  	  	loading : 'loading',
+      	title : 'Sign in',
+    	content  : '<div class="ui form segment" style="display: block;"><div class="field"><label>Email address</label><div class="ui left labeled icon input"><input type="text" placeholder="Email address"><i class="user icon"></i><div class="ui corner label"><i class="icon asterisk"></i></div>          </div>        </div>        <div class="field">          <label>Password</label>          <div class="ui left labeled icon input">            <input type="password" placeholder="Password" required>            <i class="lock icon"></i>            <div class="ui corner label">              <i class="icon asterisk"></i>            </div>          </div>        </div>      <div class="inline field">               </div>      <div ng-click="login()" class="ui blue small submit button">Sign in</div></div>'	 
+	 });
+	$scope.login = function(){
+		alert("alert");
+	}
   });
